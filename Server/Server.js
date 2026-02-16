e7d0aa5980fb90102639261af002636962676772
Srajan Narsingh <Srajannarsingh42@gmail.com>
2026-02-16 11:57:03 +0530

    2nd day

--- Server/Server.js
+++ Server/Server.js
@@ -1,7 +1,30 @@
 const http =require("http");
+const fs = require("fs")
+
+const users={
+    "Name":"Srajan Narsingh",
+    "Roll No":"65"
+}
 
 const server = http.createServer((req, res) => {
-    res.end('Hi This Is New');
+    const url=req.url;
+    const method=req.method;
+    if (url==="/users" && method==="GET"){
+        res.writeHead(200,{"Content-Type":"application/json"});
+        res.end(JSON.stringify(users));
+    }
+    else if (method === "POST" && url === "/users") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", ()=> {
+        fs.writeFile("./file,txt", body, () => {
+            res.writeHead(201, "written");
+            res.end();
+        })
+    });
+  }
 });
 
 server.listen(3000,() => {console.log("Server running on port 3000");
