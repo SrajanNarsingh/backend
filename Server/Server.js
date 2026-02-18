fbb279c4f6f850c292fcca385b167ca698b150ab
Srajan Narsingh <Srajannarsingh42@gmail.com>
2026-02-18 11:46:14 +0530

    new

--- Server/Server.js
+++ Server/Server.js
@@ -9,6 +9,12 @@ const users={
 const server = http.createServer((req, res) => {
     const url=req.url;
     const method=req.method;
+    const log = `${new Date().toISOString()} ${req.method} ${req.url}\n`;
+
+    fs.appendFile("server.log", log, (err) => {
+    if (err) console.error(err);
+    });
+
     if (url==="/users" && method==="GET"){
         res.writeHead(200,{"Content-Type":"application/json"});
         res.end(JSON.stringify(users));
@@ -19,7 +25,7 @@ const server = http.createServer((req, res) => {
       body += chunk;
     });
     req.on("end", ()=> {
-        fs.writeFile("./file,txt", body, () => {
+        fs.writeFile("./file.txt", body, () => {
             res.writeHead(201, "written");
             res.end();
         })
